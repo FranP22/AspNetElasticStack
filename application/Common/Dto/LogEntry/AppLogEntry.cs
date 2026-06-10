32ada235c21d3444370c8a2f1e79843c720e6139
FranP <fprizma000@gmail.com>
2026-06-10 13:56:56 +0200

    Updated directory name

---
+++ application/Common/Dto/LogEntry/AppLogEntry.cs
@@ -0,0 +1,27 @@
+﻿namespace Common.Dto.Log
+{
+    public class AppLogEntry
+    {
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+        public string Level { get; set; } = "Information";
+
+        public string Message { get; set; } = string.Empty;
+
+        public string? Service { get; set; }
+
+        public string? Environment { get; set; }
+
+        public string? Ip { get; set; }
+
+        public string? UserId { get; set; }
+
+        public string? Endpoint { get; set; }
+
+        public string? TraceId { get; set; }
+
+        public Exception? Exception { get; set; }
+
+        public Dictionary<string, object>? Properties { get; set; }
+    }
+}
